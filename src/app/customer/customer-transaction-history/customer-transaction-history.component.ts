b73eb6a3d9f50c203c27ee1b2abc2041c5735baa
hegde-atri <iamatrihegde@gmail.com>
2021-11-30 12:07:50 +0000

    ~changing system kernel

--- src/app/customer/customer-transaction-history/customer-transaction-history.component.ts
+++ src/app/customer/customer-transaction-history/customer-transaction-history.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+];
+
 @Component({
   selector: 'ba-customer-transaction-history',
   templateUrl: './customer-transaction-history.component.html',
-  styleUrls: ['./customer-transaction-history.component.scss']
+  styleUrls: ['./customer-transaction-history.component.scss'],
 })
 
 export class CustomerTransactionHistoryComponent implements OnInit {
-  
-  pageTitle: string = "Transaction History";
+  pageTitle: string = 'Transaction History';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void { }
-
+  ngOnInit(): void {}
 }
