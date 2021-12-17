4771436f166b9ee80c369253b1a13c8a4fff4628
Atri Hegde <iamatrihegde@gmail.com>
2021-12-17 12:07:05 +0000

    loginworks-with correct domain

--- src/app/auth-config.ts
+++ src/app/auth-config.ts
@@ -22,20 +22,20 @@ const isIE =
  */
 export const b2cPolicies = {
   names: {
-    signUpSignIn: 'B2C_1_signin-up',
-    editProfile: 'B2C_1_edit-profile',
+    signUpSignIn: 'B2C_1_signInUp',
+    editProfile: 'B2C_1_editProfile',
   },
   authorities: {
     signUpSignIn: {
       authority:
-        'https://superstonksBanking.b2clogin.com/superstonksBanking.onmicrosoft.com/B2C_1_signin-up',
+        'https://superstonks.b2clogin.com/superstonks.onmicrosoft.com/B2C_1_signInUp',
     },
     editProfile: {
       authority:
-        'https://superstonksBanking.b2clogin.com/superstonksBanking.onmicrosoft.com/B2C_1_edit-profile',
+        'https://superstonks.b2clogin.com/superstonks.onmicrosoft.com/B2C_1_editProfile',
     },
   },
-  authorityDomain: 'superstonksBanking.b2clogin.com',
+  authorityDomain: 'superstonks.b2clogin.com',
 };
 
 /**
@@ -47,10 +47,10 @@ export const msalConfig: Configuration = {
   auth: {
     // clientId: '8439ed2b-ecfd-4e48-85d7-1863b37abb98', // This is the ONLY mandatory field that you need to supply.
     // authority: b2cPolicies.authorities.signUpSignIn.authority, // Defaults to "https://login.microsoftonline.com/common"
-    clientId: '8439ed2b-ecfd-4e48-85d7-1863b37abb98', // This is the ONLY mandatory field that you need to supply.
-    authority: 'https://login.microsoftonline.com/f3d0f3eb-fb9e-4014-bcba-e45ae50522a1', // Defaults to "https://login.microsoftonline.com/common"
+    clientId: '7c6dfea2-ff7b-4e36-8b11-08410e69f4e2', // This is the ONLY mandatory field that you need to supply.
+    authority: 'https://superstonks.b2clogin.com/superstonks.onmicrosoft.com/B2C_1_signInUp' , // Defaults to "https://login.microsoftonline.com/common"
     knownAuthorities: [b2cPolicies.authorityDomain], // Mark your B2C tenant's domain as trusted.
-    redirectUri: 'http://localhost:4200/home', // Points to window.location.origin. You must register this URI on Azure portal/App Registration.
+    redirectUri: 'http://localhost:4200', // Points to window.location.origin. You must register this URI on Azure portal/App Registration.
     postLogoutRedirectUri: '/', // Indicates the page to navigate after logout.
     navigateToLoginRequestUrl: true, // If "true", will navigate back to the original request location before processing the auth code response.
   },
@@ -77,3 +77,11 @@ export const silentRequest = {
   scopes: ['openid', 'profile'],
   loginHint: 'example@domain.net',
 };
+
+// THese are the protected resources, that need authenication in order to be accessed
+export const protectedResources = {
+  customerApi: {
+    endpoint: "https://localhost:4200/customer/home",
+    scopes: ["https://superstonks.onmicrosoft.com/7c6dfea2-ff7b-4e36-8b11-08410e69f4e2/Files.Customer"]
+  }
+}
--- src/app/home/home-navbar/home-navbar.component.ts
+++ src/app/home/home-navbar/home-navbar.component.ts
@@ -1,24 +1,50 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MsalBroadcastService, MsalGuardConfiguration, MsalService, MSAL_GUARD_CONFIG } from '@azure/msal-angular';
-import { AuthenticationResult, InteractionType, PopupRequest, RedirectRequest } from '@azure/msal-browser';
+import { Component, Inject, OnInit, OnDestroy} from '@angular/core';
+import { MsalService, MsalBroadcastService, MSAL_GUARD_CONFIG, MsalGuardConfiguration } from '@azure/msal-angular';
+import { EventMessage, EventType, InteractionType, InteractionStatus, PopupRequest, RedirectRequest, AuthenticationResult, AuthError } from '@azure/msal-browser';
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
 import { b2cPolicies } from 'src/app/auth-config';
+
 
 @Component({
   selector: 'ba-home-navbar',
   templateUrl: './home-navbar.component.html',
-  styleUrls: ['./home-navbar.component.scss']
+  styleUrls: ['./home-navbar.component.scss'],
 })
+
 export class HomeNavbarComponent implements OnInit {
   loginDisplay = false;
 
 
   constructor(@Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,
-              private authService: MsalService,
-              private msalBroadcastService: MsalBroadcastService) { }
+    private authService: MsalService,
+    private msalBroadcastService: MsalBroadcastService) { }
 
-  isLoggedIn(): boolean{
+  isLoggedIn(): boolean {
     return this.authService.instance.getActiveAccount() != null;
   }
+
+  // login(userFlowRequest?: RedirectRequest | PopupRequest) {
+  //   if (this.msalGuardConfig.interactionType === InteractionType.Popup) {
+  //     if (this.msalGuardConfig.authRequest) {
+  //       this.authService.loginPopup({...this.msalGuardConfig.authRequest, ...userFlowRequest} as PopupRequest)
+  //         .subscribe((response: AuthenticationResult) => {
+  //           this.authService.instance.setActiveAccount(response.account);
+  //         });
+  //     } else {
+  //       this.authService.loginPopup(userFlowRequest)
+  //         .subscribe((response: AuthenticationResult) => {
+  //           this.authService.instance.setActiveAccount(response.account);
+  //         });
+  //     }
+  //   } else {
+  //     if (this.msalGuardConfig.authRequest){
+  //       this.authService.loginRedirect({...this.msalGuardConfig.authRequest, ...userFlowRequest} as RedirectRequest);
+  //     } else {
+  //       this.authService.loginRedirect(userFlowRequest);
+  //     }
+  //   }
+  // }
 
   login() {
     this.authService.loginPopup().subscribe((response: AuthenticationResult) => {
@@ -32,5 +58,4 @@ export class HomeNavbarComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
