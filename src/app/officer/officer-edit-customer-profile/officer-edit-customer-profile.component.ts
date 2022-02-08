aee5d420d292369ff30837a8556bb7c256086009
hegde-atri <iamatrihegde@gmail.com>
2022-02-08 16:41:58 +0000

    fixed B2C editProfile

--- src/app/officer/officer-edit-customer-profile/officer-edit-customer-profile.component.ts
+++ src/app/officer/officer-edit-customer-profile/officer-edit-customer-profile.component.ts
@@ -51,7 +51,7 @@ export class OfficerEditCustomerProfileComponent implements OnInit {
   }
 
   openEditProfile(){
-    window.open("https://superstonks.b2clogin.com/superstonks.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_editProfile&client_id=7c6dfea2-ff7b-4e36-8b11-08410e69f4e2&nonce=defaultNonce&redirect_uri=http%3A%2F%2Flocalhost%3A4200&scope=openid&response_type=id_token&prompt=login");
+    window.open("https://superstonks.b2clogin.com/superstonks.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_editProfile&client_id=583f7fec-46ac-46e1-97f4-9cc9f931ad80&nonce=defaultNonce&redirect_uri=https%3A%2F%2Fsuper-stonks.herokuapp.com%2Fhome&scope=openid&response_type=id_token&prompt=login", "_blank");
   }
 
   get addresses(): FormArray {
